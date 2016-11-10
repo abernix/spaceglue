e0961b1eda08e327152e50832be15ba43d912ae3
Jesse Rosenberger <github@jro.cc>
2016-11-10 18:07:50 +0200

    Single test script

--- admin/run_tests.sh
+++ admin/run_tests.sh
@@ -1,33 +1,63 @@
-#!/bin/sh
-
-set -e
+#!/usr/bin/env bash
 
 : ${NODE_VERSION?"must be set."}
 
 my_dir=`dirname $0`
+root_dir="$my_dir/.."
 
-(
-  TEST_BUILD=true . ${my_dir}/build_it.sh
+our_scripts="\
+${my_dir}/tests/test_meteor_app.sh
+${my_dir}/tests/test_bundle_local_mount.sh
+${my_dir}/tests/test_phantomjs.sh
+${my_dir}/tests/test_no_app.sh
+"
 
-  # We should now have access to these vars, let's share them.
-  export DOCKER_IMAGE_NAME_BASE
-  export DOCKER_IMAGE_NAME_BUILDDEPS
-  export DOCKER_IMAGE_NAME_ONBUILD
+# Add meteor build versions
+our_scripts="${our_scripts}$( \
+  cat ${my_dir}/meteor_versions_to_test | \
+  xargs -n1 -I% sh -c "\
+    echo ${my_dir}/tests/test_meteor_app.sh % && \
+    echo ${my_dir}/tests/test_bundle_web.sh %
+  "\
+)"
 
-  for meteor_version in `cat ${my_dir}/meteor_versions_to_test`; do
-    ${my_dir}/tests/test_meteor_app.sh "${meteor_version}"
-  done
+if [ -z "${CIRCLE_NODE_TOTAL}" ] || [ -z "${CIRCLE_NODE_INDEX}" ]; then
+  echo "=> Running all tests.  CI parallelism not enabled."
+  our_work="${our_scripts}"
+else
+  echo "=> Automatically balancing work with CI parallelism"
+  our_work="$( \
+    echo "${our_scripts}" | \
+    awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}"
+  )"
+fi
 
-  ${my_dir}/tests/test_bundle_local_mount.sh
+if [ -z "${our_work}" ]; then
+  echo "more parallelism than tests"
+  exit 0
+fi
 
-  for meteor_version in `cat ${my_dir}/meteor_versions_to_test`; do
-    ${my_dir}/tests/test_bundle_web.sh "${meteor_version}"
-  done
+on_test_error () {
+  echo "ERROR: Some tests failed!"
+  exit 1
+}
 
-  ${my_dir}/tests/test_phantomjs.sh
-  ${my_dir}/tests/test_no_app.sh
+# Bash-ism.
+trap 'on_test_error' ERR
 
-  # Consider doing some trap cleanup here.
-)
+TEST_BUILD=true . ${my_dir}/build_it.sh
 
-set +e
+# Print a list of what we've done.
+echo "============= Built Images ============="
+echo "  base: ${DOCKER_IMAGE_NAME_BASE}"
+echo "  builddeps: ${DOCKER_IMAGE_NAME_BUILDDEPS}"
+echo "  onbuild: ${DOCKER_IMAGE_NAME_ONBUILD}"
+
+# Export them so they're available to the build scripts.
+export DOCKER_IMAGE_NAME_BASE
+export DOCKER_IMAGE_NAME_BUILDDEPS
+export DOCKER_IMAGE_NAME_ONBUILD
+echo "${our_work}" | tr '\n' '\0' | xargs -n1 -0 -I% -t sh -c "% || exit 255"
+
+trap - ERR
+echo "All tests finished successfully."
