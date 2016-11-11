f870e358c468edcd3343a2eeb1dcaac365c449e8
Jesse Rosenberger <github@jro.cc>
2016-11-11 16:06:58 +0200

    Recommend Node 4.6.1 for all Meteor 1.4.x apps
    I believe it should be compatible with all Meteor apps as there are no breaking API changes in any newer Node 4 versions.

--- images/base/scripts/lib/version-map.js
+++ images/base/scripts/lib/version-map.js
@@ -2,19 +2,25 @@ module.exports = {
   "METEOR@1.4.1.1": {
     node: "4.4.7",
     recommendedDockerImages: [
-      "abernix/meteord:base-node-4.4.7",
+      "abernix/spaceglue:node-4.6.1",
     ],
   },
   "METEOR@1.4.1.2": {
     node: "4.6.0",
     recommendedDockerImages: [
-      "abernix/meteord:base-node-4.6.0",
+      "abernix/spaceglue:node-4.6.1",
     ],
   },
   "METEOR@1.4.1.3": {
     node: "4.6.1",
     recommendedDockerImages: [
-      "abernix/meteord:base-node-4.6.1"
+      "abernix/spaceglue:node-4.6.1"
+    ],
+  },
+  "METEOR@1.4.1.3": {
+    node: "4.6.1",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.1"
     ],
   },
 };
