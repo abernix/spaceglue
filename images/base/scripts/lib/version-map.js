9720780f081995f122ed586d4aca6b6b884c8694
Jesse Rosenberger <github@jro.cc>
2016-11-13 22:57:45 +0200

    Add Support for Meteor 1.4.2.2 which will ship with Node 4.6.2

--- images/base/scripts/lib/version-map.js
+++ images/base/scripts/lib/version-map.js
@@ -3,30 +3,41 @@ module.exports = {
     node: "4.4.7",
     recommendedDockerImages: [
       "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
     ],
   },
   "METEOR@1.4.1.2": {
     node: "4.6.0",
     recommendedDockerImages: [
       "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
     ],
   },
   "METEOR@1.4.1.3": {
     node: "4.6.1",
     recommendedDockerImages: [
-      "abernix/spaceglue:node-4.6.1"
+      "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
     ],
   },
   "METEOR@1.4.1.3": {
     node: "4.6.1",
     recommendedDockerImages: [
-      "abernix/spaceglue:node-4.6.1"
+      "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
     ],
   },
   "METEOR@1.4.2.1": {
     node: "4.6.1",
     recommendedDockerImages: [
-      "abernix/spaceglue:node-4.6.1"
+      "abernix/spaceglue:node-4.6.1",
+      "abernix/spaceglue:node-4.6.2",
+    ],
+  },
+  "METEOR@1.4.2.2": {
+    node: "4.6.2",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-4.6.2",
     ],
   },
 };
