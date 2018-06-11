83fffc15dbd04605a7956b756a642b8eb3dcf314
Jesse Rosenberger <git@jro.cc>
2018-06-11 16:30:21 +0300

    Node 8.11.2 and Meteor 1.7.0.1.

--- images/base/scripts/lib/version-map.js
+++ images/base/scripts/lib/version-map.js
@@ -138,15 +138,21 @@ module.exports = {
     ],
   },
   "METEOR@1.6": {
-    node: "8.9.3",
+    node: "8.11.2",
     recommendedDockerImages: [
-      "abernix/spaceglue:node-8.9.3",
+      "abernix/spaceglue:node-8.11.2",
     ],
   },
   "METEOR@1.6.0.1": {
     node: "8.9.3",
     recommendedDockerImages: [
-      "abernix/spaceglue:node-8.9.3",
+      "abernix/spaceglue:node-8.11.2",
+    ],
+  },
+  "METEOR@1.7.0.1": {
+    node: "8.11.2",
+    recommendedDockerImages: [
+      "abernix/spaceglue:node-8.11.2",
     ],
   },
 };
